eac6ac039d457d5a7196770afc7e1b819e3aa454
WCB <webcodingby@yandex.by>
2020-11-07 00:00:28 +0300

    add fonts and basic skelet project file

--- Web Core/webpack/webpack.config.js
+++ Web Core/webpack/webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
 
     // Кладем стили в отдельный файлик
     new MiniCssExtractPlugin({
-      filename: 'style.css',
+      filename: 'css/style.css',
     }),
 
     // Копируем картинки
